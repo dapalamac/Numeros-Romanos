8c38fbb8e9e067d7560ae5247698dc0f181f8adb
David Palacio <dapalma94@gmail.com>
2022-02-19 22:32:01 -0500

    Nuevo Cambio-2

--- NUMROM.py
+++ NUMROM.py
@@ -17,7 +17,7 @@ numrom = {
         'M': "1000",
         }  
 
-num = 'IV'
+num = 'III'
 
 nuevalista = [str(a) for a in str(num)]
 print('-----proceso1-----')
@@ -26,10 +26,14 @@ print(nuevalista)
 print('-----proceso2-----')
 
 sum = 0
+
+listarenovada = []
 for x in nuevalista:
     
     
     print(numrom[x])
+    
+    listarenovada.append((numrom[x]))
     
     sum = sum + int(numrom[x])
 
@@ -37,6 +41,91 @@ for x in nuevalista:
 print('-----proceso3-----')  
 
 print(sum)
+
+print('-----proceso4-----')  
+
+res = 0
+
+for i in range (len(listarenovada)):
+    
+    if int(listarenovada[0]) < int(listarenovada[1]):
+        
+       sum = -(int(listarenovada[0]) - int(listarenovada[1]))
+    
+    elif int(listarenovada[0]) >= int(listarenovada[1]):
+        
+       sum = int(listarenovada[0]) + int(listarenovada[1])
+       
+       
+       
+       
+    elif int(listarenovada[1]) < int(listarenovada[2]):
+        
+        sum = (int(listarenovada[0]) + int(listarenovada[1])) - (int(listarenovada[2]))
+        
+    elif int(listarenovada[1]) >= int(listarenovada[2]):
+        
+        sum = int(listarenovada[0]) + int(listarenovada[1]) + int(listarenovada[2])
+        
+        
+        
+        
+  
+    elif int(listarenovada[2]) < int(listarenovada[3]):
+        
+        sum = (int(listarenovada[0]) + int(listarenovada[1])+ int(listarenovada[2])) - (int(listarenovada[3]))
+        
+    elif int(listarenovada[2]) >= int(listarenovada[3]):
+        
+        sum = int(listarenovada[0]) + int(listarenovada[1])+ int(listarenovada[2]) + int(listarenovada[3])
+        
+    
+    elif int(listarenovada[3]) < int(listarenovada[4]):
+        
+        sum = (int(listarenovada[0]) + int(listarenovada[1])+ int(listarenovada[2])+ int(listarenovada[3])) - (int(listarenovada[4]))
+        
+    elif int(listarenovada[3]) >= int(listarenovada[4]):
+        
+        sum = (int(listarenovada[0]) + int(listarenovada[1])+ int(listarenovada[2])+ int(listarenovada[3]) + int(listarenovada[4]))
+                                                                  
+                                                                  
+                                                                  
+   
+                                                               
+
+ 
+
+                                                               
+                                                               
+                                                               
+                                                               
+        
+    # elif int(listarenovada[3]) < int(listarenovada[4]):
+        
+    #     sum = (int(listarenovada[0]) + int(listarenovada[1])+ int(listarenovada[2])+ int(listarenovada[3])) - (int(listarenovada[4])) 
+        
+    # elif int(listarenovada[4]) < int(listarenovada[5]):
+        
+    #     sum = (int(listarenovada[0]) + int(listarenovada[1])+ int(listarenovada[2])+ int(listarenovada[3])+ int(listarenovada[4])) - (int(listarenovada[5])) 
+    
+    
+print(sum)
+    
+   
+        
+        
+        
+        
+         
+         
+         
+
+
+
+
+
+    
+    
 
     
 
